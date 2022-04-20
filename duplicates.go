1dc0038496cea5ede11811c6fe3c1279b1ccac15
defia <defiaq@gmail.com>
2022-04-20 23:02:53 +0800

    add remove duplicates

---
+++ duplicates.go
@@ -0,0 +1,25 @@
+package golib
+
+func RemoveDuplicateStr(strSlice []string) []string {
+	allKeys := make(map[string]bool)
+	list := []string{}
+	for _, item := range strSlice {
+		if _, value := allKeys[item]; !value {
+			allKeys[item] = true
+			list = append(list, item)
+		}
+	}
+	return list
+}
+
+func RemoveDuplicateInt(intSlice []int) []int {
+	allKeys := make(map[int]bool)
+	list := []int{}
+	for _, item := range intSlice {
+		if _, value := allKeys[item]; !value {
+			allKeys[item] = true
+			list = append(list, item)
+		}
+	}
+	return list
+}
