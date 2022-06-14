e48edd048422e954705aa3b6b78ac49819640d6f
defia <defiaq@gmail.com>
2022-06-14 09:54:03 +0800

    export a new function for excel parse

--- excel.go
+++ excel.go
@@ -9,7 +9,8 @@ import (
 	"github.com1/tealeg/xlsx"
 )
 
-func ParseExcel(filename, sheetname string, headerLine int, obj interface{}) error {
+func ParseExcel2(sheet *xlsx.Sheet, headerLine int, obj interface{}) error {
+	indecColumnMap := make(map[int]string)
 	arrobj := reflect.ValueOf(obj).Elem()
 
 	t := reflect.TypeOf(obj).Elem().Elem().Elem()
@@ -27,61 +28,82 @@ func ParseExcel(filename, sheetname string, headerLine int, obj interface{}) err
 		m[tag] = field.Name
 	}
 
+	for j, row := range sheet.Rows {
+
+		if j < headerLine {
+			continue
+		}
+		if j == headerLine {
+			for i, cell := range row.Cells {
+
+				header := cell.String()
+				log.Println(header)
+				if v, has := m[header]; has {
+					indecColumnMap[i] = v
+				}
+
+			}
+			// log.Println(indecColumnMap)
+			// spew.Dump(m)
+		} else {
+			if len(row.Cells) < 0 {
+				continue
+			}
+			newobj := reflect.New(t)
+			for index, fieldName := range indecColumnMap {
+				if index >= len(row.Cells) {
+					continue
+				}
+				field := newobj.Elem().FieldByName(fieldName)
+				switch field.Kind() {
+				case reflect.Int:
+					tempint := strings.TrimSpace(row.Cells[index].String())
+					i, err := strconv.Atoi(tempint)
+					if err != nil {
+						log.Println("line："+strconv.Itoa(j+1)+" column:"+strconv.Itoa(index+1)+" int error:"+tempint, err)
+					}
+					field.SetInt(int64(i))
+				case reflect.String:
+					field.SetString(row.Cells[index].String())
+				}
+
+			}
+			arrobj.Set(reflect.Append(arrobj, (newobj)))
+
+		}
+
+	}
+
+	// newobj := reflect.New(t)
+	// for _, v := range m {
+
+	// 	field := newobj.Elem().FieldByName(v)
+	// 	switch field.Kind() {
+	// 	case reflect.Int:
+	// 		field.SetInt(8)
+	// 	case reflect.String:
+	// 		field.SetString("yes")
+	// 	}
+
+	// }
+	// arrobj.Set(reflect.Append(arrobj, (newobj)))
+	// arr := reflect.ArrayOf(0, t)
+
+	return nil
+}
+func ParseExcel(filename, sheetname string, headerLine int, obj interface{}) error {
+
 	xlFile, err := xlsx.OpenFile(filename)
 	if err != nil {
 		log.Fatal(err)
 	}
 	// m := make(map[string]bool)
-	indecColumnMap := make(map[int]string)
 	for _, sheet := range xlFile.Sheets {
 		if sheet.Name != sheetname {
 			// log.Println(sheet.Name)
 			continue
 		}
-		for j, row := range sheet.Rows {
-
-			if j < headerLine {
-				continue
-			}
-			if j == headerLine {
-				for i, cell := range row.Cells {
-
-					header := cell.String()
-					log.Println(header)
-					if v, has := m[header]; has {
-						indecColumnMap[i] = v
-					}
-
-				}
-				// log.Println(indecColumnMap)
-				// spew.Dump(m)
-			} else {
-				if len(row.Cells) < 0 {
-					continue
-				}
-				newobj := reflect.New(t)
-				for index, fieldName := range indecColumnMap {
-					if index >= len(row.Cells) {
-						continue
-					}
-					field := newobj.Elem().FieldByName(fieldName)
-					switch field.Kind() {
-					case reflect.Int:
-						tempint := strings.TrimSpace(row.Cells[index].String())
-						i, err := strconv.Atoi(tempint)
-						if err != nil {
-							log.Println("line："+strconv.Itoa(j+1)+" column:"+strconv.Itoa(index+1)+" int error:"+tempint, err)
-						}
-						field.SetInt(int64(i))
-					case reflect.String:
-						field.SetString(row.Cells[index].String())
-					}
-
-				}
-				arrobj.Set(reflect.Append(arrobj, (newobj)))
-
-			}
-		}
+		return ParseExcel2(sheet, headerLine, obj)
 	}
 
 	// newobj := reflect.New(t)
